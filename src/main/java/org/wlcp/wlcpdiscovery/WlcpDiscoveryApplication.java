0c6187306df27bf8a26180ad70e17d7b8bb75abe
mmicciolo <mmicciolo@wpi.edu>
2019-12-17 23:10:23 -0500

    Initial Commit

---
+++ src/main/java/org/wlcp/wlcpdiscovery/WlcpDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlcpDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlcpDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wlcp/wlcpdiscovery/WlcpDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcpdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlcpDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
