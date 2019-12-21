698064c3bb078ceabb17830d6355aa7b1dd2ae86
mmicciolo <mmicciolo@wpi.edu>
2019-12-20 20:36:42 -0500

    Added Eureka Service.

--- src/main/java/org/wlcp/wlcpdiscovery/WlcpDiscoveryApplication.java
+++ src/main/java/org/wlcp/wlcpdiscovery/WlcpDiscoveryApplication.java
@@ -2,8 +2,10 @@ package org.wlcp.wlcpdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WlcpDiscoveryApplication {
 
 	public static void main(String[] args) {
